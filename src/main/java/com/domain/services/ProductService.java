3adb144d5a15589843d36512efe057284d5ad440
Rido Atmanto <rido.atmanto@dayadimensi.co.id>
2021-12-15 15:35:07 +0700

    Initial crud

---
+++ src/main/java/com/domain/services/ProductService.java
@@ -0,0 +1,45 @@
+package com.domain.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import com.domain.models.entities.Product;
+import com.domain.models.repos.ProductRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public Product save(Product product){
+        return productRepo.save(product);
+    }
+
+    public Product findOne(Long id){
+        Optional<Product> result = productRepo.findById(id);
+        if(!result.isPresent()){
+            return null;
+        }
+        return result.get();
+    }
+
+    public Iterable<Product> findAll(){
+        return productRepo.findAll();
+    }
+
+    public void removeOne(Long id){
+        productRepo.deleteById(id);
+    }
+
+    public List<Product> findByName(String name){
+        return productRepo.findByNameContains(name);
+    }
+
+}
